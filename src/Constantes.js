d2bd19fa5f863bd998e5061710fd7e8497adea5b
Juanpahesi <120343466+Juanpahesi@users.noreply.github.com>
2022-12-11 21:51:50 -0500

    Add files via upload
    JUANPABLOWEB

---
+++ src/Constantes.js
@@ -0,0 +1,34 @@
+
+const restaurantes= [ 
+    {
+        id: "1",
+        nombre: "Cabaña El Sabor",
+        descrip: "Carrera 27 Nro. 28 - A1 Bogotá \n Deliciosa Platos Exquisitos",
+        url: require(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT15H5Uv5U-EuxKmIweGhDXZAKVL6N1dxBgw&usqp=CAU`)
+    },
+    {
+        id: "2",
+        nombre: "DELICIAS RUTA DEL SABOR 55",
+        descrip: "Av Oriental 24 Nro. 5 - 44 Villa de Leyva \n Deliciosa comida Típica",
+        url: require(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPV19XAbGeGN91b6VM7jLPokG77mmNKQbVg&usqp=CAU`)
+    },
+    {
+        id: "3",
+        nombre: "Sabinos Pizzas y Más",
+        descrip: "AV Colon #21 a 25 Tunja \n Platos a la Carta y Comidas rápidas",
+        url: require(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYauf1Sy4glMijYAiDjM2NF3QJb_dg4W2-jA&usqp=CAU`)
+
+    },
+    {
+        id: "4",
+        nombre: "KRISBY DELICIAS",
+        descrip: "Calle 19 # 20 - 64 Sáchica Boyacá \n Exquisito Pollo Asado y Broaster",
+        url: require(`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG9Ry90W_GXby2OXEXN4ggXOOucCVWPkkUVg&usqp=CAU`)
+    }
+];
+
+
+
+export{
+    restaurantes
+};
